8b223f4547c924e8e5bf808a63b9004be526b341
Zaouat <zaouatabdellah@gmail.com>
2024-08-07 14:33:00 +0100

    add userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,23 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
      <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
+     {/*02 <div>
       <Header />
       <MainContent />
       <Footer />
-    </div>
-    {/* <WelcomeMessage />
-      <div>
+    </div> */}
+
+    {/*01-question <WelcomeMessage /> */}
+      { /* 00-question <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>

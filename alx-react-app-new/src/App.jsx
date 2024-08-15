754226384499a984107fcbfdaf10714059d3ef7e
Zaouat <zaouatabdellah@gmail.com>
2024-08-15 12:49:26 +0100

    add counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,19 +7,26 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+
+import Counter from './components/Counter';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <div>
+    <div>
+      <h1>Simple Counter App</h1>
+      <Counter />
+    </div>
+     {/* <div>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
      02 <div>
       <Header />
       <MainContent />
       <Footer />
-    </div>
+    </div> */}
 
     {/*01-question <WelcomeMessage /> */}
       { /* 00-question <div>

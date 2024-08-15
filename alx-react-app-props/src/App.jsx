0e8209db6cb9836d93cc113aca3edcd87dbf35f4
Zaouat <zaouatabdellah@gmail.com>
2024-08-15 13:00:39 +0100

    move usercontext to component

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import ProfilePage from './components/UserProfile';
-import UserContext from './contexts/UserContext';
+import UserContext from './components/UserContext';
 
 function App() {
   const [count, setCount] = useState(0)

09af0d8007d0513da29d7144b834e7d9f8cddfe9
Zaouat <zaouatabdellah@gmail.com>
2024-08-26 23:54:32 +0100

    jnlkòn

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -10,10 +10,13 @@ const fetchPosts = async () => {
 };
 
 function PostsComponent() {
-  const { data, error, isLoading, refetch } = useQuery("posts", fetchPosts);
+  const { data, error, isError, isLoading, refetch } = useQuery(
+    "posts",
+    fetchPosts
+  );
 
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  if (isError) return <div>Error: {error.message}</div>;
 
   return (
     <div>

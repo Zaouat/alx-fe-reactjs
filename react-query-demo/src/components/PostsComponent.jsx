f428db4c12c0f9984fe7c79f5c09e5107f271817
Zaouat <zaouatabdellah@gmail.com>
2024-08-26 23:56:21 +0100

    add timeout

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -12,7 +12,13 @@ const fetchPosts = async () => {
 function PostsComponent() {
   const { data, error, isError, isLoading, refetch } = useQuery(
     "posts",
-    fetchPosts
+    fetchPosts,
+    {
+      cacheTime: 1000 * 60 * 5, // 5 minutes
+      staleTime: 1000 * 60 * 1, // 1 minute
+      refetchOnWindowFocus: false,
+      keepPreviousData: true,
+    }
   );
 
   if (isLoading) return <div>Loading...</div>;

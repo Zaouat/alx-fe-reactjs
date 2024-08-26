71c6184680b1c30c811898c872e3425e1e5ef275
Zaouat <zaouatabdellah@gmail.com>
2024-08-27 00:07:34 +0100

    add useAuth

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-function ProtectedRoute({ isAuthenticated, children }) {
+// Mock hook for authentication status
+const useAuth = () => {
+  // Logic to determine if the user is authenticated
+  return { isAuthenticated: false };
+};
+
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+
   return isAuthenticated ? children : <Navigate to="/" />;
 }
 

76845cfca6c83eee2ab198bb8f44b9c8e2b50b43
Zaouat <zaouatabdellah@gmail.com>
2024-09-02 12:28:35 +0100

    fix tailwind config

--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+});

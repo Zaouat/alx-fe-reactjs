18cd75754356682f14ef0319ca55e3481c1b49c1
Zaouat <zaouatabdellah@gmail.com>
2024-09-02 12:23:45 +0100

    cdkm

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,16 +2,16 @@
 
 function UserProfile() {
   return (
-    <div className="w-full max-w-md bg-gray-100 p-4 sm:p-4 md:p-8 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 mx-auto"
+        className="rounded-full w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
       />
-      <h1 className="text-xl sm:text-2xl text-blue-800 mt-4 mb-2 text-center">
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center">
         John Doe
       </h1>
-      <p className="text-gray-600 text-sm sm:text-base text-center">
+      <p className="text-gray-600 text-sm md:text-base text-center">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>

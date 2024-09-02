2913d3a4bf1600b15a76bab82e04ef4b5b841148
Zaouat <zaouatabdellah@gmail.com>
2024-09-02 12:29:46 +0100

    fix tailwaind config

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -4,7 +4,12 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}", // Targets all JavaScript/TypeScript files in the src directory
     "./public/index.html", // Includes the main HTML file
   ],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [require("tailwindcss"), require("autoprefixer")],
--- tailwind-react-integration/vite.config.js
+++ tailwind-react-integration/vite.config.js
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
 });
